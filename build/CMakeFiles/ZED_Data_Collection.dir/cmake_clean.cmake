c5269b5c62aaf567733a648feafa4df99d852f52
rockkingjy <rockking.jy@gmail.com>
2017-11-20 16:56:16 +0900

    First commit

---
+++ build/CMakeFiles/ZED_Data_Collection.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZED_Data_Collection.dir/src/main.o"
+  "CMakeFiles/ZED_Data_Collection.dir/src/SaveDepth.o"
+  "ZED_Data_Collection.pdb"
+  "ZED_Data_Collection"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZED_Data_Collection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
